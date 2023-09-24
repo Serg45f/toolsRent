42f37ad612453bd61199c3c6ce8028e31e9a9ca0
Serhii_Savchenko <sgsavch75@gmail.com>
2023-09-24 13:49:52 +0300

    First  commit

---
+++ src/main/java/com/sgsavch/toolsrent/ToolsrentApplication.java
@@ -0,0 +1,13 @@
+package com.sgsavch.toolsrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsrentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgsavch/toolsrent/ToolsrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgsavch.toolsrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
